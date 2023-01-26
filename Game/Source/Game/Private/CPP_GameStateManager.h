9f5018ddcd237279e4ff46e2199ac5e89409ebe1
Rian Rutherford <rr262471@falmouth.ac.uk>
2023-01-26 17:29:21 +0000

    Added game camera and game instance manager

---
+++ Game/Source/Game/Private/CPP_GameStateManager.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "CPP_GameStateManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UCPP_GameStateManager : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
